34d6759e801ba0b6611fd346b056b4ab8a3003e2
AnkitKrGautam <ankitkumargautam@smartdatainc.net>
2023-09-21 13:01:26 +0530

    pure recursion

--- recursion.js
+++ recursion.js
@@ -69,3 +69,23 @@ console.log(
   "collectOdds([1,2,3,4,5,6,7,8,9]) -- ",
   collectOdds([1, 2, 3, 4, 5, 6, 7, 8, 9])
 );
+
+//Pure Recursion
+function collectOddValues(arr) {
+  let newArr = [];
+  if (arr.length === 0) {
+    return newArr;
+  }
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+
+  return newArr;
+}
+
+console.log(
+  "collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]) ==> ",
+  collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+);
