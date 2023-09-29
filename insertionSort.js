a5a91d79155362bfae80c80458c90f7d7230a046
AnkitKrGautam <ankitkumargautam@smartdatainc.net>
2023-09-29 17:44:07 +0530

    insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,28 @@
+function InsertionSort(arr) {
+  var currentVal;
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+
+  return arr;
+}
+
+console.log("Insertion sort", InsertionSort([5, 4, 23, 3, 21]));
+
+function insertionSort(arr) {
+  var currentVal;
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+insertionSort([2, 1, 9, 76, 4]);
